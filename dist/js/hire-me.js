dccd224ac149ec5c3dccb75b93170a7f60dcd24a
atomgroomdesign <atom@atomgroom.com>
2015-06-10 14:50:32 -0500

    fixes last commit that broke other animations

--- dist/js/hire-me.js
+++ dist/js/hire-me.js
@@ -330,6 +330,20 @@ var queryMD = "screen and (min-width:48em)";
 enquire.register(queryMD, function() {
   var wow = new WOW(
     {
+      boxClass:     'scroll-one',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      }
+    }
+  );
+  wow.init();  
+  var wow = new WOW(
+    {
       boxClass:     'scroll-icon',      // animated element css class (default is wow)
       animateClass: 'animated', // animation css class (default is animated)
       offset:       100,          // distance to the element when triggering the animation (default is 0)
@@ -341,18 +355,5 @@ enquire.register(queryMD, function() {
       }
     }
   );
-  var wow = new WOW(
-    {
-      boxClass:     'scroll-one',      // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       true,       // trigger animations on mobile devices (default is true)
-      live:         true,       // act on asynchronously loaded content (default is true)
-      callback:     function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      }
-    }
-  );  
   wow.init();
 });
